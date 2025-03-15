1b0e954ac92366f1784f99de58b6e6aee3d63caf
Anton Rie <riea50206@gmail.com>
2025-03-14 20:04:19 -0400

    Added sfx

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -4,6 +4,7 @@ extends RigidBody3D
 var deactivated = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	AudioManager.play("res://assets/sounds/laserShoot.wav")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
